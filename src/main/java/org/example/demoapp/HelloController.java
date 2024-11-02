7013fa6c1cf74e06c1acea906919266c57d73857
Yoe <dembouz719@gmail.com>
2024-11-02 18:24:14 +0100

    updated CrossOrigin

--- src/main/java/org/example/demoapp/HelloController.java
+++ src/main/java/org/example/demoapp/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @CrossOrigin(origins = "https://y00e.github.io/reactdemoapp/")
+    @CrossOrigin(origins = "https://y00e.github.io")
     @GetMapping("/hello")
     public String helloWorld() {
         return "Hello, World!";
