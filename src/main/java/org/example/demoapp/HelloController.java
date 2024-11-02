24f01f53840858ef243a54ed6a058cf46c5091dc
Yoe <dembouz719@gmail.com>
2024-11-02 16:53:44 +0100

    Initial commit

---
+++ src/main/java/org/example/demoapp/HelloController.java
@@ -0,0 +1,15 @@
+package org.example.demoapp;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @CrossOrigin(origins = "http://localhost:5173/")
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "Hello, World!";
+    }
+}
